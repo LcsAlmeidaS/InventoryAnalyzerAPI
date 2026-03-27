f4a0e2b0a2a6af9bfb12c048a3b35573d772e9dc
LcsAlmeidaS <lcshenriquegs@gmail.com>
2026-03-27 16:30:38 -0300

    feat: add upload size limit and register services by interface.

--- InventoryAnalyzerAPI/Program.cs
+++ InventoryAnalyzerAPI/Program.cs
@@ -1,12 +1,20 @@
+using InventoryAnalyzerAPI.Services;
+using InventoryAnalyzerAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Http.Features;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<CsvParseService>();
-builder.Services.AddScoped<InventoryService>();
+builder.Services.Configure<FormOptions>(options =>
+{
+    options.MultipartBodyLengthLimit = 10 * 1024 * 1024;
+});
+
+builder.Services.AddScoped<ICsvParseService, CsvParseService>();
+builder.Services.AddSingleton<IInventoryService, InventoryService>();
 
 var app = builder.Build();
 
@@ -17,9 +25,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
