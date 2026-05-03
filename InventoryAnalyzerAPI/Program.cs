4a4de3a833c6a21ee0fccdadc04f2e462ae39aa1
Lucas Almeida <lcshenriquegs@gmail.com>
2026-05-03 20:08:21 -0300

    feat: add global exception handler returning ProblemDetails on 500.

--- InventoryAnalyzerAPI/Program.cs
+++ InventoryAnalyzerAPI/Program.cs
@@ -7,26 +7,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddProblemDetails();
+
+var allowedOrigins = builder.Configuration
+    .GetSection("Cors:AllowedOrigins")
+    .Get<string[]>() ?? [];
 
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins(allowedOrigins)
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
 });
 
+var maxFileSizeBytes = builder.Configuration
+    .GetValue<long>("Upload:MaxFileSizeBytes", 10 * 1024 * 1024);
+
 builder.Services.Configure<FormOptions>(options =>
 {
-    options.MultipartBodyLengthLimit = 10 * 1024 * 1024;
+    options.MultipartBodyLengthLimit = maxFileSizeBytes;
 });
 
 builder.Services.AddScoped<ICsvParseService, CsvParseService>();
-builder.Services.AddSingleton<IInventoryService, InventoryService>();
+builder.Services.AddScoped<IInventoryService, InventoryService>();
 
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 if (app.Environment.IsDevelopment())
 {
